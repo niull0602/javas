fad893771dea257e45493a3466ad6d47122ef0b6
niu <lelement@163.com>
2019-12-05 15:26:13 +0800

    first

---
+++ day02/src/demo1/Dormitory.java
@@ -0,0 +1,7 @@
+package demo1;
+
+/**
+ * Created by lemon on 2019-12-05 15:20.
+ */
+public class Dormitory {
+}
---
+++ day02/src/demo1/Student.java
@@ -0,0 +1,55 @@
+package demo1;
+
+import java.util.Date;
+
+/**
+ * Created by lemon on 2019-12-05 13:44.
+ */
+public class Student {
+    private Integer id;
+    private String name;
+    private String sex;
+    private Date birthday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
